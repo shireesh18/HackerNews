4c62c8c1b3357d93d1083cd88429e69cd5b8c53e
Shireesh <shireesh18@gmail.com>
2024-03-20 20:03:24 +0000

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Shireesh Marla on 20/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
